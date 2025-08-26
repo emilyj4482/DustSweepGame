16caa89d9fce0815fb2d0d630ff60b88bdd5bf47
emilyj4482 <emilyj4482@gmail.com>
2025-08-27 01:28:44 +0900

    [chore] replaced all resources to cat-concept

---
+++ DustSweepGame/Cat.swift
@@ -0,0 +1,16 @@
+//
+//  Cat.swift
+//  DustSweepGame
+//
+//  Created by EMILY on 27/08/2025.
+//
+
+import Foundation
+
+enum Cat: String, CaseIterable {
+    case cheese
+    case mackerel
+    case gray
+    case vanila
+    case calico
+}
